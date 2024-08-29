51292e5b95fbb680d24f233960d0d26752f3041d
hrasek <hosekmotor@seznam.cz>
2024-08-29 15:20:00 +0200

    Added email notifications.

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -36,8 +37,11 @@ class SendEventReminders extends Command
 
         $events->each(
             fn ($event) => $event->attendees->each(
-                fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")));
-
+                fn ($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
+            )
+        );
         $this->info('Reminder notifications send successfully!');
     }
 }
